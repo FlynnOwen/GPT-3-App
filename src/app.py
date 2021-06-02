bbebf9397eb838d7f97eef82353f12aaa4557a74
Flynn Owen <flynnoo49@gmail.com>
2021-06-02 21:05:17 +1200

    Many fixes in app.py - save messages to database, capture timestamps

--- src/app.py
+++ src/app.py
@@ -1,22 +1,26 @@
 import random
 import os
+import time
 
 from flask import Flask, request
 from pymessenger.bot import Bot
 
+import database.db_test_scripts as db
+
 app = Flask(__name__)
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-VERIFY_TOKEN = os.getenv('TESTINGTOKEN')
+VERIFY_TOKEN = os.getenv('VERIFY_TOKEN')
 bot = Bot(ACCESS_TOKEN)
 
 
 # We will receive messages that Facebook sends our bot at this endpoint
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
+    print(os.getcwd())
     # Get verify token to ensure requests are legitimate
     if request.method == 'GET':
         token_sent = request.args.get("hub.verify_token")
-        return verify_fb_token(token_sent)
+        return _verify_fb_token(token_sent)
 
     # Post request from user when a message is received
     else:
@@ -28,38 +32,45 @@ def receive_message():
             for message in messaging:
                 if message.get('message'):
 
-                    # Cache Sender Id
-                    recipient_id = message['sender']['id']
-
-                    # Cache message received
+                    # Cache metadata and payload
+                    member_id = message['sender']['id']
                     rec_message = message['message'].get('text')
+                    rec_timestamp = message['timestamp']
+
+                    # Save in database
+                    db.add_message(rec_message, member_id, rec_timestamp, 'received')
 
                     if rec_message:
-                        response_sent_text = get_message()
-                        send_message(recipient_id, response_sent_text)
+                        response_sent_text = _get_message()
+                        _send_message(member_id, response_sent_text)
 
                     # If user sends us a GIF, photo,video, or any other non-text item
                     if message['message'].get('attachments'):
-                        send_message(recipient_id, "My brain is not yet big enough to deal with images!")
+                        _send_message(member_id, "My brain is not yet big enough to deal with images!")
 
     return "Message Processed"
 
 
-def verify_fb_token(token_sent):
+def _verify_fb_token(token_sent):
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
 
     return 'Invalid verification token'
 
 
-def get_message():
+def _get_message():
     sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
 
     return random.choice(sample_responses)
 
 
-def send_message(recipient_id, response):
-    bot.send_text_message(recipient_id, response)
+def _send_message(member_id, response):
+    sent_timestamp = int(time.time() * 1000)
+
+    # Save in database
+    db.add_message(response, member_id, sent_timestamp, 'sent')
+
+    bot.send_text_message(member_id, response)
 
     return "success"
 
