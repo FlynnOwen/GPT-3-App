6de5dddf4fd06ddb4be839bc09f79cafba704ac8
Flynn Owen <flynnoo49@gmail.com>
2021-06-06 22:08:46 +1200

    database updates

--- src/prompt_design.py
+++ src/prompt_design.py
@@ -1,36 +1,32 @@
-input_prompt = input("Type message you would like to send: ")
+import os
+import openai
 
-default_prompt = """The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.
+DEFAULT_PROMPT = """The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.
 
 Human: Hello, who are you?
 AI: I am an AI created by OpenAI. How can I help you today?"""
 
-full_prompt = default_prompt + "\nHuman: " + input_prompt + "\n"
+def gen_response(input_prompt):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
-import os
-import openai
+    full_prompt = DEFAULT_PROMPT + "\nHuman: " + input_prompt + "\nAI:"
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+    response = openai.Completion.create(
+      engine="davinci",
+      prompt=full_prompt,
+      temperature=0.9,
+      max_tokens=150,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0.6,
+      stop=["\n", " Human:", " AI:"]
+    )
 
-response = openai.Completion.create(
-  engine="davinci",
-  prompt=full_prompt,
-  temperature=0.9,
-  max_tokens=150,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0.6,
-  stop=["\n", " Human:", " AI:"]
-)
+    response_text = response.get('choices')[0].get('text')
 
-response_text = response.get('choices')[0].get('text')
-print(response)
-print(response_text)
+    return response_text
 
-print(full_prompt)
-
-print(full_prompt + response_text)
-
+'''
 second_prompt = full_prompt + response_text
 
 second_input_prompt = input()
@@ -51,3 +47,4 @@ response_text = response.get('choices')[0].get('text')
 print(response_text)
 
 print(second_full_prompt + response_text)
+'''
