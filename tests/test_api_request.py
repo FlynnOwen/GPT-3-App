638fa2280567a36f28f52a02b14d60093d5bc208
Flynn Owen <flynnoo49@gmail.com>
2021-10-02 13:48:00 +1300

    Add test for request from OpenAI

---
+++ tests/test_api_request.py
@@ -0,0 +1,27 @@
+import os
+
+from dotenv import load_dotenv
+import pytest
+import openai
+
+
+def test_open_ai_response():
+    load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    try:
+        response = openai.Completion.create(
+          engine="davinci",
+          prompt="test",
+          temperature=0.9,
+          max_tokens=150,
+          top_p=1,
+          frequency_penalty=0,
+          presence_penalty=0.6,
+          stop=["\n", " Human:", " AI:"]
+        )
+
+    except:
+        pytest.fail("An error occurred with OpenAI request")
+
+test_open_ai_response()
