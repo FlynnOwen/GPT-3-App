5d68e186f58baa29b2f01a98a829e02199f16742
Flynn Owen <flynnoo49@gmail.com>
2021-10-02 23:58:41 +1300

    Test DB connection

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,6 +1,30 @@
+import os
+
 import pytest
+import psycopg2
+from dotenv import load_dotenv
 
 from database.db_test_scripts import add_message, add_user, most_recent_message_timestamp, get_current_conversation
 
+load_dotenv()
+DATABASE_URL = os.environ['DATABASE_URL']
 
-#def test_add_message():
+
+def test_db_connection():
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    cur = conn.cursor()
+
+    # Use a list here to insert query parameters into the query string.
+    cur.execute(
+        """
+        SELECT * 
+        FROM messages
+        """
+    )
+
+    result = cur.fetchone()
+
+    print(result)
+
+    cur.close()
